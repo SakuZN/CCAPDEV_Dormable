5cb7f14b4d5375cc8de1c2a1aa484fc0d222c963
Ranzreka <zach_noche@dlsu.edu.ph>
2023-07-01 10:13:19 +0800

    Figured out reviews can be sorted, will do a proper implementation

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,17 +1,36 @@
-const getUserData = function (userData) {
+//Variable for swiper
+let mySwiper;
+
+//variable for the user profile
+let userProfile;
+
+//Object function for user profile
+const getUserData = function (userData, isCurrentUser) {
   this.user = userData;
-  this.userReviewHistory = getUserReviews(this.user.username);
-  this.divRH = populateHistoryAsDiv(this.userReviewHistory, this.user);
+  this.isOwnProfile = isCurrentUser;
+  this.userRHData = populateHistoryAsDiv(getUserReviews(this.user.username), this.user, isCurrentUser);
 }
-// TODO: Create another Object class for divRH for easy access to the divs
-function populateHistoryAsDiv(reviewHistory, reviewUser) {
+
+//Object function for review history
+const reviewHistoryData = function (reviewHistory, divRH) {
+  this.reviewHistory = reviewHistory;
+  this.divRH = divRH;
+}
+
+//Turns review history into divs and returns an array of review history objects
+function populateHistoryAsDiv(reviewHistory, reviewUser, isCurrentUser) {
   let userReviewHistory = [];
   let listingDatabaseString = JSON.parse(localStorage.getItem('listingDatabase'));
+
   reviewHistory.forEach((review) => {
 
     let swiperDiv = document.createElement('div');
     swiperDiv.classList.add('swiper-slide');
+    swiperDiv.setAttribute('data-review-id', review.reviewID);
+    swiperDiv.setAttribute('data-listing-id', review.listingID);
+    swiperDiv.setAttribute('data-user-id', review.userID);
     let listingName = listingDatabaseString.find(listing => listing.id === review.listingID).name;
+
     let scoreClass = '';
     let checkEdit = '';
     if (review.wasEdited)
@@ -24,7 +43,6 @@ function populateHistoryAsDiv(reviewHistory, reviewUser) {
       scoreClass = 'customer-rating red';
 
     swiperDiv.innerHTML = `
-       <div class="swiper-slide">
         <div class="customer-review_wrap">
             <div class="customer-img">
             <img src="${reviewUser.profilePic}" class="img-fluid" alt="#">
@@ -49,14 +67,24 @@ function populateHistoryAsDiv(reviewHistory, reviewUser) {
             </ul>
             <div class="mark-helpful" data-review-id="${review.reviewID}" data-listing-id="${review.listingID}">
                 <span class="like-count">${review.reviewMarkedHelpful}</span> people marked this review as helpful
-                <button class="editReviewBtn btn btn-sm btn-primary">Edit</button>
-                <button class="confirmModal deleteReviewBtn btn btn-sm btn-primary red-btn">Delete</button>
+                ${
+                isCurrentUser? `
+                  <button class="editReviewBtn btn btn-sm btn-primary">Edit</button>
+                  <button class="confirmModal deleteReviewBtn btn btn-sm btn-primary red-btn">Delete</button>`
+                  :`
+                  <button class="button">
+                     <div class="hand">
+                        <div class="thumb"></div>
+                     </div>
+                     <span>Like<span>d</span></span>
+                  </button>`
+                  }
             </div>
             </div>
         </div>
-        <hr>
-       </div> `;
-    userReviewHistory.push(swiperDiv);
+        <hr>`;
+    let rhData = new reviewHistoryData(review, swiperDiv);
+    userReviewHistory.push(rhData);
   });
   return userReviewHistory;
 }
@@ -70,8 +98,43 @@ function populateUserReviewImg(images) {
   });
   return reviewImgs;
 }
-function populateUserReviewHistory(reviewHistory, swiperContainer) {
-  reviewHistory.forEach((review) => { swiperContainer.append(review) });
+function populateUserReviewHistory(reviewHistory) {
+  let swiperContainer = document.getElementById('reviewHistory');
+  reviewHistory.forEach((review) => { swiperContainer.append(review.divRH) });
+  initSwiper();
+}
+
+function initSwiper(){
+  mySwiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    loop: false,
+    loopFillGroupWithBlank: false,
+    pagination: {
+      el: "#review-pagination",
+      clickable: true,
+    },
+    allowTouchMove: false,
+  });
+}
+
+function testSortReviews(){
+  userProfile.userRHData.sort((a,b) => {
+    return new Date(b.reviewHistory.reviewDate) - new Date(a.reviewHistory.reviewDate);
+  });
+  destroySwiper();
+  clearReviewHistory();
+  populateUserReviewHistory(userProfile.userRHData);
+}
+
+
+function clearReviewHistory(){
+  let reviewHistory = document.getElementById('reviewHistory');
+  reviewHistory.innerHTML = '';
+}
+function destroySwiper(){
+  mySwiper.destroy();
 }
 
 function parseDate(date) {
@@ -99,8 +162,7 @@ function parseDate(date) {
 
 function populateProfile(userID, currentUser) {
   let database = JSON.parse(localStorage.getItem('userDatabase'));
-  let userData = new getUserData(database.find(user => user.username === userID));
-  console.log(userData.divRH);
+  userProfile = new getUserData(database.find(user => user.username === userID), currentUser);
 
   //Get the needed elements
   let profilePic = document.getElementById('userPic');
@@ -117,16 +179,18 @@ function populateProfile(userID, currentUser) {
   let formCollege = document.getElementById('input-college');
   let formDescription = document.getElementById('input-description');
   let reviewHistory = document.getElementById('reviewHistory');
+  let reviewPagination = document.getElementById('review-pagination');
+  let noReviews = document.createElement('div');
 
   //Get user data
-  let userPic = userData.user.profilePic;
-  let name = userData.user.username;
-  let followers = userData.user.followers;
-  let reviews = userData.user.noOfReviews;
-  let courseName = userData.user.course;
-  let userDescription = userData.user.description;
-  let collegeName = userData.user.college;
-  let dateJoined = parseDate(userData.user.joinDate);
+  let userPic = userProfile.user.profilePic;
+  let name = userProfile.user.username;
+  let followers = userProfile.user.followers;
+  let reviews = userProfile.user.noOfReviews;
+  let courseName = userProfile.user.course;
+  let userDescription = userProfile.user.description;
+  let collegeName = userProfile.user.college;
+  let dateJoined = parseDate(userProfile.user.joinDate);
 
 
   //Set user data
@@ -141,19 +205,29 @@ function populateProfile(userID, currentUser) {
   formCourse.value = courseName;
   formCollege.value = collegeName;
   formDescription.value = userDescription;
+
+
+  //Hides follow button if user is viewing their own profile
+  if (currentUser) {
+    followButton.style.display = 'none';
+    noReviews.innerHTML = 'You have not written any reviews yet!';
+  }
+  //Hides edit button if user is not viewing their own profile
+  else {
+    editButton.style.display = 'none';
+    noReviews.innerHTML = 'This user has not written any reviews yet!';
+  }
+
+  //Finally, populate the review history
   try {
-    populateUserReviewHistory(userData.divRH, reviewHistory);
+    populateUserReviewHistory(userProfile.userRHData);
   }
   catch (e) {
     console.log(e);
-  }
-
-  //Finally, some conditional logic when viewing your own profile
-  if (currentUser) {
-    followButton.style.display = 'none';
-  }
-  else {
-    editButton.style.display = 'none';
+    reviewPagination.style.display = 'none';
+    reviewHistory.style.display = 'flex';
+    reviewHistory.style.justifyContent = 'center';
+    reviewHistory.append(noReviews);
   }
 }
 
@@ -185,17 +259,17 @@ async function updateProfile() {
 //Check in profile.html if the id is valid
 if (window.location.href.includes('profile.html')) {
   let url = new URL(window.location.href);
-  let userID = url.searchParams.get('id');
-  let checkValidUser = JSON.parse(localStorage.getItem('userDatabase')).find(user => user.username === userID);
+  let profileID = url.searchParams.get('id');
+  let checkValidUser = JSON.parse(localStorage.getItem('userDatabase')).find(user => user.username === profileID);
   if (checkValidUser === undefined || checkValidUser === null) {
     window.location.href = '404.html';
   }
   else {
     let isCurrentUser = false;
     if (localStorage.getItem('isLoggedIn') === 'true') {
-      isCurrentUser = JSON.parse(localStorage.getItem('currentUser')).username === userID;
+      isCurrentUser = JSON.parse(localStorage.getItem('currentUser')).username === profileID;
     }
-    populateProfile(userID, isCurrentUser);
+    populateProfile(profileID, isCurrentUser);
     if (isCurrentUser){
       const editForm = document.getElementById('editProfile');
       editForm.addEventListener('submit', function (e) {
@@ -285,6 +359,9 @@ $(document).ready(function() {
     let files = $(this)[0].files;
     let imageList = $('.user-image-list');
     let maxFiles = 5;
+    let hasVideo = false;
+    let maxImageSize = 2 * 1024 * 1024; // 5MB
+    let maxVideoSize = 5 * 1024 * 1024; // 5MB
 
     //if current image list is not empty check if the new files exceed the max files
     if (imageList.children().length > 0) {
@@ -292,11 +369,36 @@ $(document).ready(function() {
         showPopup('Max 5 media allowed!');
         return;
       }
+      else {
+        imageList.children().each(function() {
+          if ($(this).data('type') === 'video')
+            hasVideo = true;
+        });
+      }
+
     }
 
     // Iterate over selected files
     for (let i = 0; i < files.length && i < maxFiles; i++) {
       let file = files[i];
+      let fileSize = file.size;
+      //Do some conditional pre-check
+
+      //Check if the file is video and if there is already a video
+      if (file.type.includes('video/') && hasVideo) {
+        showPopup('Max 1 video allowed!');
+        return;
+      }
+      //Then, check for file size
+      if (fileSize > maxImageSize && !file.type.includes('video/')) {
+        showPopup('Max 2MB per image!');
+        return;
+      }
+      if (fileSize > maxVideoSize && file.type.includes('video/')) {
+        showPopup('Max 5MB per video!');
+        return;
+      }
+
       let reader = new FileReader();
 
       // Read the file as a data URL
@@ -383,4 +485,53 @@ $(document).ready(function() {
       console.log("Cancel clicked");
     });
   });
+
+  //Like button
+  $('.button').on('click', function() {
+    let $button = $(this);
+    let $review = $button.closest('.mark-helpful');
+    //get the parent of the button
+    let $reviewContainer = $review.closest('.swiper-slide')
+    let $likeCount = $review.find('.like-count');
+    let reviewID = $reviewContainer.data('review-id');
+    let listingID = $reviewContainer.data('listing-id');
+    let currentCount = parseInt($likeCount.text(), 10);
+
+    if ($button.hasClass('liked')) {
+      // Decrement the like count if already liked
+      $likeCount.text(currentCount - 1);
+      reviewMarkedHelpful(reviewID, listingID, -1)
+    } else {
+      // Increment the like count if not liked
+      $likeCount.text(currentCount + 1);
+      reviewMarkedHelpful(reviewID, listingID, 1)
+    }
+
+    // Toggle the 'liked' class on button
+    $button.toggleClass('liked');
+
+    // Animate the button using GSAP
+    if ($button.hasClass('liked')) {
+      gsap.fromTo($button[0], {
+        '--hand-rotate': 8
+      }, {
+        ease: 'none',
+        keyframes: [{
+          '--hand-rotate': -45,
+          duration: 0.16,
+          ease: 'none'
+        }, {
+          '--hand-rotate': 15,
+          duration: 0.12,
+          ease: 'none'
+        }, {
+          '--hand-rotate': 0,
+          duration: 0.2,
+          ease: 'none',
+          clearProps: true
+        }]
+      });
+    }
+  });
+
 });
