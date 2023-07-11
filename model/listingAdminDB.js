a8cf2e40e723e9f195a265b2c679c4e978b835dc
Ranzreka <zach_noche@dlsu.edu.ph>
2023-07-11 11:16:01 +0800

    Reintegrated new models and routers

--- model/listingAdminDB.js
+++ model/listingAdminDB.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const listingAdminSchema = new mongoose.Schema(
     {
@@ -8,5 +9,12 @@ const listingAdminSchema = new mongoose.Schema(
     },
     { collection: "listingAdminDatabase" }
 );
+
+listingAdminSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
 
 module.exports = mongoose.model("listingAdminDatabase", listingAdminSchema);
--- model/userDB.js
+++ model/userDB.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-  {
-      username: { type: String, required: true, unique: true},
-      customName: { type: String, required: true },
-      course: { type: String, required: false },
-      college: { type: String, required: true },
-      type: { type: String, required: true },
-      description: { type: String, required: false },
-      profilePic: { type: String, default: "/blank_profile.png" },
-      joinDate: { type: Date, default: Date.now },
-      noOfReviews: { type: Number, default: 0 },
-      followers: { type: Number, default: 0 },
-      liked: [
-        {
-          reviewID: { type: Number, required: true },
-          listingID: { type: Number, required: true },
-          userID: { type: String, required: true },
-        },
-        // ... more liked reviews ...
-      ],
-  following: [String],
-   { collection: "userDatabase" }
-  }
+    {
+        username: { type: String, required: true, unique: true },
+        customName: { type: String, required: true },
+        course: { type: String, required: false },
+        college: { type: String, required: true },
+        type: { type: String, required: true },
+        description: { type: String, required: false },
+        profilePic: { type: String, default: "/blank_profile.png" },
+        joinDate: { type: Date, default: Date.now },
+        noOfReviews: { type: Number, default: 0 },
+        followers: { type: Number, default: 0 },
+        liked: [
+            {
+                reviewID: { type: Number, required: true },
+                listingID: { type: Number, required: true },
+                userID: { type: String, required: true },
+            },
+            // ... more liked reviews ...
+        ],
+        following: [String],
+    },
+    { collection: "userDatabase" }
 );
 
 module.exports = mongoose.model("userDatabase", userSchema);
