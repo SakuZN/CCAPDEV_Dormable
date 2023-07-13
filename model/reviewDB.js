2455e77834bdb1c7db4807d04009dc2ca9620e4b
louiscadaaa <louis_cada@dlsu.edu.ph>
2023-07-13 18:08:32 +0800

    reviewDB

---
+++ model/reviewDB.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+    {
+        reviewID: { type: Number, required: true },
+        userID: { type: String, required: true },
+        listingID: { type: String, required: true },
+        reviewTitle: { type: String, required: true },
+        reviewContent: { type: String, required: true },
+        reviewIMG: { type: String, required: true },
+        reviewScore: { type: Number, required: true },
+        reviewDate: { type: Date, required: true },
+        reviewMarkedHelpful: { type: Number, required: true },
+        wasEdited: { type: Boolean, required: false },
+        isDeleted: { type: Boolean, required: false }
+    },
+    { collection: "review" }
+);
+
+module.exports = mongoose.model("review", reviewSchema);
